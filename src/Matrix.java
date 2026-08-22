e6b8f0dd40d9df9ecf4e20e34744e676da9ad574
Gustavdoiss <gustavo.eduacc@gmail.com>
2026-08-22 16:05:43 -0300

    First root commit, MATRIX Class

---
+++ src/Matrix.java
@@ -0,0 +1,31 @@
+public class Matrix {
+    private final int rows, cols;
+    private final double[][] elements;
+
+    public Matrix(int rows, int cols, double[][] elements) {
+        this.rows = rows;
+        this.cols = cols;
+        this.elements = new double[rows][cols];
+
+        for (int i = 0; i < elements.length; i++) {
+            for (int j = 0; j < elements[0].length; j++) set(i+1, j+1, elements[i][j]);
+        }
+    }
+
+    public void set(int i, int j, double value) {
+        if (i > rows || i <= 0 || j > cols || j <= 0) {
+            System.out.println("Não foi possível definir um valor!");
+        }else {
+            this.elements[i-1][j-1] = value;
+        }
+    }
+
+    public double get(int i, int j) {
+        if (i > rows || i <= 0 || j > cols || j <= 0) {
+            System.out.println("Não foi possível definir um valor!");
+            return 0;
+        }else {
+            return this.elements[i-1][j-1];
+        }
+    }
+}
